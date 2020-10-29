e6f6ad43c055dade47d7f8f5513f1e525ea33976
Riccardo Massidda <contact@rmassidda.it>
2020-10-29 12:51:07 +0100

    Working prototype

---
+++ script.js
@@ -0,0 +1,184 @@
+const encodeGetParams = p => Object.entries(p).map(kv => kv.map(encodeURIComponent).join("=")).join("&");
+const base_url = 'http://it.wikipedia.org/w/api.php?'
+const count_edits = (arr, val) => arr.reduce((a, v) => (v === val ? a + 1 : a), 0)
+
+// Global state
+var state = {}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function get_revisions(r){
+  let tmp = r["query"]["pages"]
+  k = Object.keys(tmp)[0]
+  return tmp[k]["revisions"]
+}
+
+async function retrieve_history(title) {
+  // Initial parameters for the API query
+  var params = {
+    action: 'query',
+    continue: '',
+    format: 'json',
+    prop: 'revisions',
+    rvlimit: 'max',
+    rvprop: 'timestamp|user',
+    origin: '*',
+    titles: title
+  }
+  
+  // Multiple requests may be needed
+  let stop = false
+
+  // Accumulate the results
+  total = []
+  while (!stop) {
+    // Get the JSON file from the API
+    let url = base_url + encodeGetParams(params)
+    console.log('API url:',url)
+    let response = await fetch(url)
+    if (response.ok) {
+      response = await response.json();
+    } else {
+      alert("HTTP-Error: " + response.status);
+    }
+
+    // Extract data and accumulate
+    revs    = get_revisions(response)
+    total   = total.concat(revs)
+    
+    // Eventually do another call
+    if (Object.keys(response).includes("continue")) {
+      params["continue"]   = response["continue"]["continue"]
+      params["rvcontinue"] = response["continue"]["rvcontinue"]
+    }
+    else{
+      stop = true;
+    }
+
+    // Be nice!
+    await sleep(500)
+  }
+  return total
+}
+
+async function edits_per_user(title){
+  var count   = {}
+  epu = await retrieve_history(title)
+  if (epu[0] == undefined){
+    return count
+  }
+  epu     = epu.map(e => e["user"])
+  uniq    = Array.from(new Set(epu))
+  for (user of uniq){
+    count[user] = count_edits(epu, user)
+  }
+  return count
+}
+
+function update_view(titles){
+  // Select the table
+  thead = document.getElementById("table_header")
+  thead.innerHTML = ""
+
+  // Username
+  var node = document.createElement("th")
+  var txt  = document.createTextNode("Utente")
+  node.appendChild(txt)
+  node["scope"] = "col"
+  thead.appendChild(node)
+
+  // Titles
+  for (k of titles){
+    var node = document.createElement("th")
+    var txt  = document.createTextNode(k)
+    node.appendChild(txt)
+    node["scope"] = "col"
+    thead.appendChild(node)
+  }
+
+  // Sum
+  var node = document.createElement("th")
+  var txt  = document.createTextNode("Somma")
+  node.appendChild(txt)
+  node["scope"] = "col"
+  thead.appendChild(node)
+
+  // Get unique users list
+  users = []
+  for (k of titles) {
+    users = users.concat(Object.keys(state[k]))
+  }
+  users = new Set(users)
+  users = Array.from(users)
+  console.log(users)
+
+  // Aggregate
+  let agg = u => {
+    return titles.map(k => (state[k][u]!=undefined)?state[k][u]:0)
+  }
+
+  // Sort users
+  users.sort((a,b) => {
+    let sum = (x,y) => x+y
+    return - (agg(a).reduce(sum,0) - agg(b).reduce(sum,0))
+  })
+
+  // Insert data
+  tbody = document.getElementById("table_body")
+  tbody.innerHTML = ""
+  for (i in users){
+    let u = users[i]
+    // Row
+    row = document.createElement("tr")  
+
+    // Username
+    th = document.createElement("th")
+    th["scope"] = "row"
+    th.appendChild(document.createTextNode(u))
+    row.appendChild(th)
+
+    // Counters
+    for (k of titles){
+      td = document.createElement("td")
+      td.appendChild(document.createTextNode((state[k][u]!=undefined)?state[k][u]:0))
+      row.appendChild(td)
+    }
+
+    // Append row
+    tbody.appendChild(row)
+  }
+}
+
+async function update_state(e){
+  titles = document.getElementById("titles").value.split(",")
+  for (title of titles){
+
+    // Check if the object has already been downloaded
+    if (Object.keys(state).indexOf(title) > -1){
+      console.log(title,"already cached.")
+      continue;
+    }
+
+    edits = await edits_per_user(title)
+    console.log(title,"cached.")
+
+    // Check if there is at least one result
+    if (Object.keys(edits).length == 0){
+      continue;
+    }
+
+    state[title] = edits
+  }
+  update_view(titles)
+}
+
+// Event listeners
+document.getElementById("launch").addEventListener("click", update_state)
+document.getElementById("titles").addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("launch").click();
+  }
+});
